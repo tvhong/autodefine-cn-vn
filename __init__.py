9bbd93c86e739e32fe2ffbd98bed3b0a81d33c19
Vy Hong <shiweistg@gmail.com>
2025-10-12 21:14:34 +0100

    Add init and manifest.json

---
+++ __init__.py
@@ -0,0 +1,45 @@
+"""
+AutoDefine Chinese-Vietnamese Anki Addon
+
+This addon automatically populates Chinese vocabulary cards with Vietnamese translations,
+pinyin pronunciation, definitions, and audio pronunciation.
+"""
+
+from aqt import mw
+from aqt.utils import showInfo
+from aqt.qt import QAction
+from anki.hooks import addHook
+
+# Import core modules when they are created
+# from .core.translator import ChineseVietnameseTranslator
+# from .core.field_manager import FieldManager
+# from .core.ui_hooks import setup_ui_hooks
+# from .utils.config_manager import ConfigManager
+
+__version__ = "1.0.0"
+__author__ = "AutoDefine CN-VN Team"
+
+def on_addon_loaded():
+    """Initialize the addon when Anki loads."""
+    # TODO: Initialize configuration manager
+    # TODO: Setup UI hooks for card editor
+    # TODO: Register keyboard shortcuts
+    showInfo("AutoDefine Chinese-Vietnamese addon loaded successfully!")
+
+def setup_menu():
+    """Setup addon menu in Anki's Tools menu."""
+    action = QAction("AutoDefine CN-VN Settings", mw)
+    action.triggered.connect(show_settings)
+    mw.form.menuTools.addAction(action)
+
+def show_settings():
+    """Show addon configuration dialog."""
+    # TODO: Implement settings dialog
+    showInfo("Settings dialog will be implemented in future tasks.")
+
+# Initialize addon when Anki profile is loaded
+addHook("profileLoaded", on_addon_loaded)
+
+# Setup menu when main window is available
+if mw:
+    setup_menu()
